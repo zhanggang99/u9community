a18b9f791ac3262f4c1b4182a71bb50e13a1d9f2
zhanggang <zhanggang@outlook.com>
2020-02-16 21:29:42 +0800

    init

---
+++ src/main/java/com/zhanggang/u9community/U9communityApplication.java
@@ -0,0 +1,13 @@
+package com.zhanggang.u9community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U9communityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U9communityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhanggang/u9community/U9communityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhanggang.u9community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U9communityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
